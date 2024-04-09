8a86e23ac00a825a5c663ac8950ed51f37b1b0c8
Matvey Sushinsky <m.sushinsky@gmail.com>
2024-04-09 22:53:19 +0200

    Add db support

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -1,43 +1,102 @@
 -- CreateTable
-CREATE TABLE "customers" (
-    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
-    "name" VARCHAR(255) NOT NULL,
-    "email" VARCHAR(255) NOT NULL,
-    "image_url" VARCHAR(255) NOT NULL,
+CREATE TABLE "administrator" (
+    "bankovniucet" VARCHAR(50) NOT NULL,
+    "plat" DOUBLE PRECISION NOT NULL,
+    "administratorid" SERIAL NOT NULL,
 
-    CONSTRAINT "customers_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "pk_administrator" PRIMARY KEY ("administratorid")
 );
 
 -- CreateTable
-CREATE TABLE "invoices" (
-    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
-    "customer_id" UUID NOT NULL,
-    "amount" INTEGER NOT NULL,
-    "status" VARCHAR(255) NOT NULL,
-    "date" DATE NOT NULL,
+CREATE TABLE "mechanik" (
+    "bankovniucet" VARCHAR(50) NOT NULL,
+    "plat" DOUBLE PRECISION NOT NULL,
+    "specializace" VARCHAR(50) NOT NULL,
+    "mechanikid" SERIAL NOT NULL,
 
-    CONSTRAINT "invoices_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "pk_mechanik" PRIMARY KEY ("mechanikid")
 );
 
 -- CreateTable
-CREATE TABLE "revenue" (
-    "month" VARCHAR(4) NOT NULL,
-    "revenue" INTEGER NOT NULL
+CREATE TABLE "osoba" (
+    "email" VARCHAR(50) NOT NULL,
+    "jmeno" VARCHAR(50) NOT NULL,
+    "prijmeni" VARCHAR(50) NOT NULL,
+    "telcislo" VARCHAR(50) NOT NULL,
+    "osobaid" SERIAL NOT NULL,
+
+    CONSTRAINT "pk_osoba" PRIMARY KEY ("osobaid")
 );
 
 -- CreateTable
-CREATE TABLE "users" (
-    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
-    "name" VARCHAR(255) NOT NULL,
-    "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
+CREATE TABLE "rezervace" (
+    "cas" TIME(0) NOT NULL,
+    "cena" DOUBLE PRECISION NOT NULL,
+    "datum" DATE NOT NULL,
+    "rezervaceid" SERIAL NOT NULL,
+    "mechanikid" INTEGER,
+    "vozidloid" INTEGER NOT NULL,
 
-    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "pk_rezervace" PRIMARY KEY ("rezervaceid")
 );
 
--- CreateIndex
-CREATE UNIQUE INDEX "revenue_month_key" ON "revenue"("month");
+-- CreateTable
+CREATE TABLE "role" (
+    "roleid" SERIAL NOT NULL,
+    "osobaid" INTEGER NOT NULL,
 
--- CreateIndex
-CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+    CONSTRAINT "pk_role" PRIMARY KEY ("roleid")
+);
+
+-- CreateTable
+CREATE TABLE "sluzba" (
+    "nazev" VARCHAR(50) NOT NULL,
+    "odhadovanadobaprovedeni" VARCHAR(50),
+    "pribliznacena" DOUBLE PRECISION,
+    "sluzbaid" SERIAL NOT NULL,
+    "rezervaceid" INTEGER NOT NULL,
+
+    CONSTRAINT "pk_sluzba" PRIMARY KEY ("sluzbaid")
+);
+
+-- CreateTable
+CREATE TABLE "vozidlo" (
+    "model" VARCHAR(100) NOT NULL,
+    "spz" VARCHAR(8) NOT NULL,
+    "vozidloid" SERIAL NOT NULL,
+    "zakaznikid" INTEGER NOT NULL,
+
+    CONSTRAINT "pk_vozidlo" PRIMARY KEY ("vozidloid")
+);
+
+-- CreateTable
+CREATE TABLE "zakaznik" (
+    "zakaznikid" SERIAL NOT NULL,
+
+    CONSTRAINT "pk_zakaznik" PRIMARY KEY ("zakaznikid")
+);
+
+-- AddForeignKey
+ALTER TABLE "administrator" ADD CONSTRAINT "fk_administrator_role" FOREIGN KEY ("administratorid") REFERENCES "role"("roleid") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "mechanik" ADD CONSTRAINT "fk_mechanik_role" FOREIGN KEY ("mechanikid") REFERENCES "role"("roleid") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "rezervace" ADD CONSTRAINT "fk_rezervace_pridelena" FOREIGN KEY ("mechanikid") REFERENCES "mechanik"("mechanikid") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "rezervace" ADD CONSTRAINT "fk_rezervace_soucasti" FOREIGN KEY ("vozidloid") REFERENCES "vozidlo"("vozidloid") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "role" ADD CONSTRAINT "fk_role_osoba" FOREIGN KEY ("osobaid") REFERENCES "osoba"("osobaid") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "sluzba" ADD CONSTRAINT "fk_sluzba_bude" FOREIGN KEY ("rezervaceid") REFERENCES "rezervace"("rezervaceid") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "vozidlo" ADD CONSTRAINT "fk_vozidlo_je" FOREIGN KEY ("zakaznikid") REFERENCES "zakaznik"("zakaznikid") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "zakaznik" ADD CONSTRAINT "fk_zakaznik_role" FOREIGN KEY ("zakaznikid") REFERENCES "role"("roleid") ON DELETE NO ACTION ON UPDATE NO ACTION;
 

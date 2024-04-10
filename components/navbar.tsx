df165943d7b97308a7388c3029df48a9adc3224f
jan-valasek7 <honza.valasek7@gmail.com>
2024-04-10 19:49:20 +0200

    Added navbar logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,9 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
+import Image from "next/image";
+import logo from '../public/logo.png'
+
 
 const links = [
   { path: "/", name: "Home" },
@@ -16,15 +19,27 @@ export default function Navbar() {
   const isLoggedIn = false; // TODO: replace this with your actual login state
 
   return (
-    <nav className="md:navbar bg-base-100 flex justify-between ">
-      <div>
+    <nav className="navbar bg-base-200 flex justify-between mb-20">
+
+<div>
+
+      <Link href="/">
+        <div className="ml-10 mr-10 relative top-4">
+          <Image
+            src={logo}
+            alt="Autoservis logo"
+            width={230}
+            className="mt"
+          />
+        </div>
+      </Link>
         {links.map((el) => (
           el.path === pathname ? (
-            <Link className="btn btn-active btn-neutral text-xl m-1" href={el.path} key={el.name}>
+            <Link className="btn btn-active btn-neutral text-xl m-1 text-slate-300" href={el.path} key={el.name}>
               {el.name}
             </Link>
           ) : (
-            <Link className="btn btn-ghost text-xl m-1" href={el.path} key={el.name}>
+            <Link className="btn btn-ghost text-xl m-1 text-slate-800" href={el.path} key={el.name}>
               {el.name}
             </Link>
           )
@@ -33,11 +48,11 @@ export default function Navbar() {
       <div>
         {/*login for employees where manager is going to be assigning reservations to emloyees and mechanics are going see their assigned reservations*/}
         {isLoggedIn ? (
-          <Link className="btn btn-active btn-neutral text-xl m-1" href="/admin">
+          <Link className="btn btn-neutral text-xl m-1" href="/admin">
             Admin
           </Link>
         ) : (
-          <Link className="btn btn-active btn-neutral text-xl m-1" href="/login">
+          <Link className="btn btn-neutral text-xl m-1" href="/login">
             Login
           </Link>
         )}

1c077bbe8e7f2528776884caf9170e8b59310614
jan-valasek7 <honza.valasek7@gmail.com>
2024-04-15 14:10:25 +0200

    Changed height of login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,8 +2,8 @@ import LoginForm from "@/components/loginForm";
 
 export default function Home() {
     return (
-      <main className="mt-[10%] mb-[10%]">
-        <h2 className="text-3xl font-bold mb-10 text-center">Vstup do zaměstnanecké zóny</h2>
+      <main className="mt-10 mb-[15%]">
+        <h2 className="text-3xl font-bold mb-20 text-center">Vstup do zaměstnanecké zóny</h2>
         <LoginForm/>
       </main>
     );

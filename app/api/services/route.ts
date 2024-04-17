165f0960c3a904a3d2e6e0229d3a19c0766467f3
Matvei Sushinskii <sushimat@fit.cvut.cz>
2024-04-18 00:23:32 +0200

    Add first api endpoint

--- app/api/services/route.ts
+++ app/api/services/route.ts
@@ -1,9 +1,8 @@
 export const dynamic = 'force-dynamic' // defaults to auto
 
 import { prisma } from "@/app/lib/data-mapper"
-import { NextResponse } from "next/server"
 
-export async function GET(request: Request) {
-    const services = prisma.sluzba.count()
-    return NextResponse.json(services)
+export async function GET() {
+    const services = await prisma.osoba.findMany()
+    return Response.json(services)
 }

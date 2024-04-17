c090f8b9a08c2788d4b7b882ed07d65de012200b
jan-valasek7 <honza.valasek7@gmail.com>
2024-04-17 14:26:13 +0200

    Added form for adding reservation on the manager side

--- app/admin/reservations/page.tsx
+++ app/admin/reservations/page.tsx
@@ -49,6 +49,28 @@ const placeholderReservations = [
     },
 ];
 
+const placeholderServices = [
+    {
+        id: 1,
+        name: 'Výměna oleje'
+    },
+    {
+        id: 2,
+        name: 'Přezutí pneumatik'
+    },
+    {
+        id: 3,
+        name: 'Diagnostika'
+    },
+];
+
+const fetchServices = async () => {
+    //TODO: replace with actual fetching services from db 
+    return new Promise<{ id: number; name: string; }[]>((resolve) => {
+        resolve(placeholderServices);
+    });
+};
+
 const fetchMechanicReservations = async (id: number) => {
     //TODO: replace with actual fetching reservations from db based on mechanic id, ordered by date and time 
     return new Promise<{ id: number; dateTime: Date; firstName: string; lastName: string; phoneNumber: string; email: string; carModel: string; serviceType: string; }[]>((resolve) => {
@@ -111,6 +133,15 @@ function MechanicPage() {
 
 const ManagerPage = () => {
     const [reservations, setReservations] = useState<any[]>([]);
+    const [services, setServices] = useState<any[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await fetchServices();
+            setServices(data);
+        };
+        fetchData();
+    }, []);
 
     useEffect(() => {
         const fetchReservations = async () => {
@@ -136,8 +167,25 @@ const ManagerPage = () => {
     };
     return (
         <div className='text-center'>
-            <h1 className='text-3xl text-center  mb-10'>Manažerova stránka:</h1>
-            <button onClick={() => addReservation()} className="inline-block mb-5 px-6 py-3 bg-yellow-500 text-white font-bold rounded hover:bg-yellow-600 transition duration-200">Přidat rezervaci</button>
+            <h1 className='text-3xl text-center my-10'>Manažerova stránka:</h1>
+            <form className='flex flex-col w-96 mx-auto p-4 text-black border-2 mb-10'>
+                    <input type="text" placeholder="Jméno" className="input input-bordered mb-4" />
+                    <input type="text" placeholder="Příjmení" className="input input-bordered mb-4" />
+                    <input type="text" placeholder="Tel" className="input input-bordered mb-4" />
+                    <input type="text" placeholder="Email" className="input input-bordered mb-4" />
+                    <input type="text" placeholder="Model auta" className="input input-bordered mb-4" />
+                    <input type="text" placeholder="SPZ" className="input input-bordered mb-4" />
+                    <select required className='mb-4'>
+                        <option value="">Objednaná služba</option>
+                        {services.map((service) => (
+                        <option key={service.id} value={service.id}>{service.name}</option>
+                            
+                    ))}
+                    </select>
+
+
+                    <button onClick={() => addReservation()} className="inline-block mb-5 px-6 py-3 bg-yellow-500 text-white font-bold rounded hover:bg-yellow-600 transition duration-200">Přidat rezervaci</button>
+                </form>
             <h1 className='text-xl text-center mb-10'>Rezervace bez přiděleného mechanika</h1>
             <table className='mx-auto'>
                 <thead>

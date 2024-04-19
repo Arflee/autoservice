fefc5009e73f86d88c57442800c12d487ca85554
byaliyah <byaliyah@fit.cvut.cz>
2024-04-19 18:44:18 +0200

    fix db generation

--- db-data/1-generate-db.sql
+++ db-data/1-generate-db.sql
@@ -1,127 +1,70 @@
-DROP TABLE IF EXISTS Administrator CASCADE;
+DROP TABLE IF EXISTS administrator CASCADE;
+DROP TABLE IF EXISTS mechanik CASCADE;
+DROP TABLE IF EXISTS osoba CASCADE;
+DROP TABLE IF EXISTS rezervace CASCADE;
+DROP TABLE IF EXISTS sluzba CASCADE;
+DROP TABLE IF EXISTS vozidlo CASCADE;
+-- End of removing
 
-DROP TABLE IF EXISTS Mechanik CASCADE;
-
-DROP TABLE IF EXISTS Osoba CASCADE;
-
-DROP TABLE IF EXISTS Rezervace CASCADE;
-
-DROP TABLE IF EXISTS Role CASCADE;
-
-DROP TABLE IF EXISTS Sluzba CASCADE;
-
-DROP TABLE IF EXISTS Vozidlo CASCADE;
-
-DROP TABLE IF EXISTS Zakaznik CASCADE;
-
-
-CREATE TABLE Administrator
-(
-	BankovniUcet VARCHAR(50) NOT NULL ,
-	Plat DOUBLE PRECISION NOT NULL,
-	AdministratorID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
-	CONSTRAINT PK_Administrator PRIMARY KEY (AdministratorID)
+CREATE TABLE administrator (
+    id_admin INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    id_osoba INTEGER NOT NULL,
+    heslo_hash VARCHAR(256) NOT NULL,
 );
+ALTER TABLE administrator ADD CONSTRAINT pk_administrator PRIMARY KEY (id_admin, id_osoba);
+ALTER TABLE administrator ADD CONSTRAINT u_fk_administrator_osoba UNIQUE (id_osoba);
 
-CREATE TABLE Mechanik
-(
-	BankovniUcet VARCHAR(50) NOT NULL ,
-	Plat DOUBLE PRECISION NOT NULL,
-	Specializace VARCHAR(50) NOT NULL,
-	MechanikID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
-	CONSTRAINT PK_Mechanik PRIMARY KEY (MechanikID)
+CREATE TABLE mechanik (
+    id_mechanik INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    id_osoba INTEGER NOT NULL,
+    heslo_hash VARCHAR(256) NOT NULL
 );
+ALTER TABLE mechanik ADD CONSTRAINT pk_mechanik PRIMARY KEY (id_mechanik, id_osoba);
+ALTER TABLE mechanik ADD CONSTRAINT u_fk_mechanik_osoba UNIQUE (id_osoba);
 
-CREATE TABLE Osoba
-(
-	Email VARCHAR(50) NOT NULL,
-	Jmeno VARCHAR(50) NOT NULL,
-	Prijmeni VARCHAR(50) NOT NULL,
-	TelCislo VARCHAR(50) NOT NULL,
-	OsobaID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
-	CONSTRAINT PK_Osoba PRIMARY KEY (OsobaID)
+CREATE TABLE osoba (
+    id_osoba INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    tel VARCHAR(256) NOT NULL,
+    jmeno VARCHAR(256) NOT NULL,
+    prijmeni VARCHAR(256) NOT NULL,
+    email VARCHAR(256) NOT NULL
 );
+ALTER TABLE osoba ADD CONSTRAINT pk_osoba PRIMARY KEY (id_osoba);
 
-CREATE TABLE Rezervace
-(
-	Cas TIME(0) NOT NULL,
-	Cena DOUBLE PRECISION NOT NULL,
-	Datum DATE NOT NULL,
-	RezervaceID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
-	MechanikID INT NULL,
-	VozidloID INT NOT NULL,
-	CONSTRAINT PK_Rezervace PRIMARY KEY (RezervaceID)
+CREATE TABLE rezervace (
+    id_rezervace INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    id_sluzba INTEGER NOT NULL,
+    id_vozidlo INTEGER NOT NULL,
+    id_osoba INTEGER NOT NULL,
+    id_mechanik INTEGER,
+    mechanik_id_osoba INTEGER,
+    datum VARCHAR(256) NOT NULL,
+    cas VARCHAR(256) NOT NULL
 );
+ALTER TABLE rezervace ADD CONSTRAINT pk_rezervace PRIMARY KEY (id_rezervace, id_sluzba, id_vozidlo, id_osoba);
+ALTER TABLE rezervace ADD CONSTRAINT c_fk_rezervace_mechanik CHECK ((id_mechanik IS NOT NULL AND mechanik_id_osoba IS NOT NULL) OR (id_mechanik IS NULL AND mechanik_id_osoba IS NULL));
 
-CREATE TABLE Role
-(
-	RoleID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
-	OsobaID INT NOT NULL,
-	CONSTRAINT PK_Role PRIMARY KEY (RoleID)
+CREATE TABLE sluzba (
+    id_sluzba INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    nazev VARCHAR(256) NOT NULL,
+    cena VARCHAR(256) NOT NULL
 );
+ALTER TABLE sluzba ADD CONSTRAINT pk_sluzba PRIMARY KEY (id_sluzba);
 
-CREATE TABLE Sluzba
-(
-	Nazev VARCHAR(50) NOT NULL,
-	OdhadovanaDobaProvedeni VARCHAR(50) NULL,
-	PribliznaCena DOUBLE PRECISION NULL,
-	SluzbaID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
-	RezervaceID INT NOT NULL,
-	CONSTRAINT PK_Sluzba PRIMARY KEY (SluzbaID)
+CREATE TABLE vozidlo (
+    id_vozidlo INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    id_osoba INTEGER NOT NULL,
+    model VARCHAR(256) NOT NULL,
+    reg_znacka VARCHAR(256) NOT NULL
 );
+ALTER TABLE vozidlo ADD CONSTRAINT pk_vozidlo PRIMARY KEY (id_vozidlo, id_osoba);
 
-CREATE TABLE Vozidlo
-(
-	Model VARCHAR(100) NOT NULL,
-	Spz VARCHAR(8) NOT NULL,
-	VozidloID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
-	ZakaznikID INT NOT NULL,
-	CONSTRAINT PK_Vozidlo PRIMARY KEY (VozidloID)
-);
+ALTER TABLE administrator ADD CONSTRAINT fk_administrator_osoba FOREIGN KEY (id_osoba) REFERENCES osoba (id_osoba) ON DELETE CASCADE;
 
-CREATE TABLE Zakaznik
-(
-	ZakaznikID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
-	CONSTRAINT PK_Zakaznik PRIMARY KEY (ZakaznikID)
-);
+ALTER TABLE mechanik ADD CONSTRAINT fk_mechanik_osoba FOREIGN KEY (id_osoba) REFERENCES osoba (id_osoba) ON DELETE CASCADE;
 
+ALTER TABLE rezervace ADD CONSTRAINT fk_rezervace_sluzba FOREIGN KEY (id_sluzba) REFERENCES sluzba (id_sluzba) ON DELETE CASCADE;
+ALTER TABLE rezervace ADD CONSTRAINT fk_rezervace_vozidlo FOREIGN KEY (id_vozidlo, id_osoba) REFERENCES vozidlo (id_vozidlo, id_osoba) ON DELETE CASCADE;
+ALTER TABLE rezervace ADD CONSTRAINT fk_rezervace_mechanik FOREIGN KEY (id_mechanik, mechanik_id_osoba) REFERENCES mechanik (id_mechanik, id_osoba) ON DELETE CASCADE;
 
-ALTER TABLE Administrator 
- ADD CONSTRAINT FK_Administrator_Role
-	FOREIGN KEY (AdministratorID) REFERENCES Role (RoleID) ON DELETE No Action ON UPDATE No Action
-;
-
-ALTER TABLE Mechanik 
- ADD CONSTRAINT FK_Mechanik_Role
-	FOREIGN KEY (MechanikID) REFERENCES Role (RoleID) ON DELETE No Action ON UPDATE No Action
-;
-
-ALTER TABLE Rezervace 
- ADD CONSTRAINT FK_Rezervace_pridelena
-	FOREIGN KEY (MechanikID) REFERENCES Mechanik (MechanikID) ON DELETE No Action ON UPDATE No Action
-;
-
-ALTER TABLE Rezervace 
- ADD CONSTRAINT FK_Rezervace_soucasti
-	FOREIGN KEY (VozidloID) REFERENCES Vozidlo (VozidloID) ON DELETE No Action ON UPDATE No Action
-;
-
-ALTER TABLE Role 
- ADD CONSTRAINT FK_Role_Osoba
-	FOREIGN KEY (OsobaID) REFERENCES Osoba (OsobaID) ON DELETE No Action ON UPDATE No Action
-;
-
-ALTER TABLE Sluzba 
- ADD CONSTRAINT FK_Sluzba_bude
-	FOREIGN KEY (RezervaceID) REFERENCES Rezervace (RezervaceID) ON DELETE No Action ON UPDATE No Action
-;
-
-ALTER TABLE Vozidlo 
- ADD CONSTRAINT FK_Vozidlo_je
-	FOREIGN KEY (ZakaznikID) REFERENCES Zakaznik (ZakaznikID) ON DELETE No Action ON UPDATE No Action
-;
-
-ALTER TABLE Zakaznik 
- ADD CONSTRAINT FK_Zakaznik_Role
-	FOREIGN KEY (ZakaznikID) REFERENCES Role (RoleID) ON DELETE No Action ON UPDATE No Action
-;
+ALTER TABLE vozidlo ADD CONSTRAINT fk_vozidlo_osoba FOREIGN KEY (id_osoba) REFERENCES osoba (id_osoba) ON DELETE CASCADE;

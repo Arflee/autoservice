3fb0785f195bb307e890e88742d6b1d54683d5b0
jan-valasek7 <honza.valasek7@gmail.com>
2024-04-19 17:56:03 +0200

    Added some placeholder data in array (as fetched from database)

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,28 +1,28 @@
 import ServiceCard from "@/components/serviceCard";
 import { Service } from "../lib/definitions";
 
-const testService: Service = {
   //should fetch from db (Service[] services = ServiceDAO.getAllServices())
-  name: "Test",
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae ligula tempor, suscipit lacus et, laoreet du",
-  short_button_text: "Buy",
-};
+const placeholderServices = [
+ {name:"Test",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae ligula tempor, suscipit lacus et, laoreet du", short_button_text:"Rezervace"},
+ {name:"Test",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae ligula tempor, suscipit lacus et, laoreet du", short_button_text:"Rezervace"},
+ {name:"Test",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae ligula tempor, suscipit lacus et, laoreet du", short_button_text:"Rezervace"},
+ {name:"Test",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae ligula tempor, suscipit lacus et, laoreet du", short_button_text:"Rezervace"},
+ {name:"Test",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae ligula tempor, suscipit lacus et, laoreet du", short_button_text:"Rezervace"},
+ {name:"Test",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae ligula tempor, suscipit lacus et, laoreet du", short_button_text:"Rezervace"},
+ {name:"Test",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae ligula tempor, suscipit lacus et, laoreet du", short_button_text:"Rezervace"},
+ {name:"Test",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae ligula tempor, suscipit lacus et, laoreet du", short_button_text:"Rezervace"},
+
+];
+
 
 export default function Page() {
   return (
     <main>
       <h1 className="text-3xl font-bold m-20 text-center">Naše služby</h1>
       <div className="grid grid-cols-4">
-        <ServiceCard service={testService} />
-        <ServiceCard service={testService} />
-        <ServiceCard service={testService} />
-        <ServiceCard service={testService} />
-        <ServiceCard service={testService} />
-        <ServiceCard service={testService} />
-        <ServiceCard service={testService} />
-        <ServiceCard service={testService} />
-        <ServiceCard service={testService} />
+      {placeholderServices.map((service)=>(
+        <ServiceCard key={service.name} service={service}/>
+      ))}
       </div>
     </main>
   );

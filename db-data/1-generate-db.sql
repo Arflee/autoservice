eab1f215d512256f0cc4f0919f6a285d514024e7
Jan Valášek <valasj13@fit.cvut.cz>
2024-04-21 19:19:26 +0200

    updated data types 'generate_db' script

--- db-data/1-generate-db.sql
+++ db-data/1-generate-db.sql
@@ -24,10 +24,10 @@ ALTER TABLE mechanik ADD CONSTRAINT u_fk_mechanik_osoba UNIQUE (id_osoba);
 
 CREATE TABLE osoba (
     id_osoba INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
-    tel VARCHAR(256) NOT NULL,
-    jmeno VARCHAR(256) NOT NULL,
-    prijmeni VARCHAR(256) NOT NULL,
-    email VARCHAR(256) NOT NULL
+    tel VARCHAR(15) NOT NULL,
+    jmeno VARCHAR(40) NOT NULL,
+    prijmeni VARCHAR(40) NOT NULL,
+    email VARCHAR(100) NOT NULL
 );
 ALTER TABLE osoba ADD CONSTRAINT pk_osoba PRIMARY KEY (id_osoba);
 
@@ -38,15 +38,15 @@ CREATE TABLE rezervace (
     id_osoba INTEGER NOT NULL,
     id_mechanik INTEGER,
     mechanik_id_osoba INTEGER,
-    datum VARCHAR(256) NOT NULL,
-    cas VARCHAR(256) NOT NULL
+    datum DATE NOT NULL,
+    cas TIME NOT NULL
 );
 ALTER TABLE rezervace ADD CONSTRAINT pk_rezervace PRIMARY KEY (id_rezervace, id_sluzba, id_vozidlo, id_osoba);
 ALTER TABLE rezervace ADD CONSTRAINT c_fk_rezervace_mechanik CHECK ((id_mechanik IS NOT NULL AND mechanik_id_osoba IS NOT NULL) OR (id_mechanik IS NULL AND mechanik_id_osoba IS NULL));
 
 CREATE TABLE sluzba (
     id_sluzba INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
-    nazev VARCHAR(256) NOT NULL,
+    nazev VARCHAR(100) NOT NULL,
     cena VARCHAR(256) NOT NULL
 );
 ALTER TABLE sluzba ADD CONSTRAINT pk_sluzba PRIMARY KEY (id_sluzba);
@@ -54,8 +54,8 @@ ALTER TABLE sluzba ADD CONSTRAINT pk_sluzba PRIMARY KEY (id_sluzba);
 CREATE TABLE vozidlo (
     id_vozidlo INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
     id_osoba INTEGER NOT NULL,
-    model VARCHAR(256) NOT NULL,
-    reg_znacka VARCHAR(256) NOT NULL
+    model VARCHAR(100) NOT NULL,
+    reg_znacka VARCHAR(8) NOT NULL
 );
 ALTER TABLE vozidlo ADD CONSTRAINT pk_vozidlo PRIMARY KEY (id_vozidlo, id_osoba);
 

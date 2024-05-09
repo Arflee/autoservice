072f54502d04bdf929dd99b9587b4eb0e24b5444
Matvei Sushinskii <sushimat@fit.cvut.cz>
2024-05-09 12:10:25 +0200

    Add linter

--- app/lib/reservationDAO.ts
+++ app/lib/reservationDAO.ts
@@ -1,6 +1,8 @@
 import { rezervace } from "@prisma/client";
 import { Reservation } from "./definitions";
-
+/**
+ * docs for `Reser`
+ */
 export interface ReservationDAO {
   GetReservationAllInfo() : Promise<Reservation[]>;
   GetAllReservations() : Promise<rezervace[]>;

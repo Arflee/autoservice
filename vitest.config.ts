29b751bd0cb05d736d464277e070fad3c125f1ad
Matvei Sushinskii <sushimat@fit.cvut.cz>
2024-05-15 22:10:00 +0200

    Add lcov format

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     coverage: {
+      reporter: ['lcov', 'html'],
       exclude: [
         ...coverageConfigDefaults.exclude,
         'docs/*',

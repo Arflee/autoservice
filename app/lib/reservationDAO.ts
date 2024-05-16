fed72020963704e1890cd352531f95753fff4f3b
Matvei Sushinskii <sushimat@fit.cvut.cz>
2024-05-16 08:00:00 +0200

    Change gitignore

--- app/lib/reservationDAO.ts
+++ app/lib/reservationDAO.ts
@@ -1,10 +1,17 @@
 import { rezervace } from "@prisma/client";
 import { Reservation } from "./definitions";
 /**
- * docs for `Reser`
+ * @description Every object that wants to receive data from db has to implement this interface
  */
 export interface ReservationDAO {
+
+/**
+ * @description Method queries to db and gets reservation info + several additional data fields
+ * @returns Awaitable promise with all reservations
+ */
   GetReservationAllInfo() : Promise<Reservation[]>;
+
+  
   GetAllReservations() : Promise<rezervace[]>;
   PostReservation(data: Reservation) : Promise<void>;
 }
